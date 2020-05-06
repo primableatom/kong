5b64753cd2b394adde38052b52f13eadac9be364
Aapo Talvensaari <aapo.talvensaari@gmail.com>
2020-05-06 20:39:15 +0300

    feat(conf) add port_map configuration option
    ### Summary
    
    Layer 4 port mapping (or port forwarding) is used a lot when running
    Kong inside a container. Common example is exposing ports 80 and 443
    externally while running Kong inside a container with internal ports
    8000 and 8443. It also allows Kong to started without elevated
    privileges as it is quite common that you need root-access to bind
    processes to TCP port < 1024.
    
    As this translation is done in many cases without using layer 4
    proxy_protocol or layer 7 X-Forwarded-Port HTTP header, the Kong has
    no knowledge about the target port that client originally connected
    to.
    
    This PR adds `port_map` configuration parameter:
    
    ```
    #port_map =                      # When running Kong behind layer 4 port mapping
                                     # (or port-forwarding), e.g. with
                                     # `docker run -p 80:8000`, you can use this
                                     # parameter to let Kong know about published
                                     # port when it differs from the one Kong is
                                     # listening, e.g. port_map=80:8000, 443:8443`.`
                                     # It has effect on `X-Forwarded-Port`, stream
                                     # routing by destination port, PDK function
                                     # `kong.request.get_forwarded_port()`, and
                                     # on basic log serializer.
    ```
    
    This gets parsed into `kong.configuration.host_ports` table.
    
    E.g. `KONG_PORT_MAP="80:8000,443:8443"` where the first number (`80`/`443`) before `:`
    is the host published port and the second one (`8000`/`8443`) is the port that Kong is
    listening translates to following `kong.configuration.host_ports`:
    
    ```lua
    {
      [8000]   = 80,
      ["8000"] = 80,
      [8443]   = 443,
      ["8443"] = 443,
    }
    ```
    
    This PR also adds a new nginx context variable `ngx.ctx.host_port` which
    contains this translated port. So instead of using `ngx.var.server_port`
    the more correct one in many cases is `ngx.ctx.host_port`.
    
    Kong PDK is also changed to take this in account when using:
    ```
    local port = kong.request.get_forwarded_port()
    ```
    
    This commit also changes the `X-Forwarded-Port` to use this. Additionally
    `stream routing` by `destination port` will use this too. Basic log
    serializer was changed to use `ngx.var.host_port` as well.
    
    Currently it is only usable in proxy/stream, so things like `admin api`
    is not affected by this.

--- spec/01-unit/03-conf_loader_spec.lua
+++ spec/01-unit/03-conf_loader_spec.lua
@@ -445,6 +445,35 @@ describe("Configuration loader", function()
     end)
   end)
 
+  describe("port_map and host_ports", function()
+    it("are empty tables when not specified", function()
+      local conf = assert(conf_loader(helpers.test_conf_path, {}))
+      assert.same({}, conf.port_map)
+      assert.same({}, conf.host_ports)
+    end)
+    it("are tables when specified", function()
+      local conf = assert(conf_loader(helpers.test_conf_path, {
+        port_map = "80:8000,443:8443",
+      }))
+      assert.same({
+        "80:8000",
+        "443:8443",
+      }, conf.port_map)
+      assert.same({
+        [8000]   = 80,
+        ["8000"] = 80,
+        [8443]   = 443,
+        ["8443"] = 443,
+      }, conf.host_ports)
+    end)
+    it("gives an error with invalid value", function()
+      local _, err = conf_loader(helpers.test_conf_path, {
+        port_map = "src:dst",
+      })
+      assert.equal("invalid port mapping (`port_map`): src:dst", err)
+    end)
+  end)
+
   describe("inferences", function()
     it("infer booleans (on/off/true/false strings)", function()
       local conf = assert(conf_loader())
